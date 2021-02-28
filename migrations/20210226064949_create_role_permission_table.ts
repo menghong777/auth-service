bf275c78ca213ad213106583308ec27b9c467bf6
Meng Hong <menghong@compass-interactive.com>
2021-03-01 07:32:52 +0800

    Update table migration

--- migrations/20210226064949_create_role_permission_table.ts
+++ migrations/20210226064949_create_role_permission_table.ts
@@ -4,8 +4,8 @@ export async function up (knex: Knex) {
   // tslint:disable-next-line: await-promise
   await knex.schema.createTable('role_permission', table => {
     table.increments('id')
-    table.integer('roleId').unsigned().index()
-    table.integer('permissionId').unsigned().index()
+    table.integer('roleId').unsigned().references('id').inTable('role')
+    table.integer('permissionId').unsigned().references('id').inTable('permission')
     table.dateTime('created').notNullable().defaultTo(knex.fn.now())
     table.dateTime('updated').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
   })
--- migrations/20210228010529_create_user_role_table.ts
+++ migrations/20210228010529_create_user_role_table.ts
@@ -4,8 +4,8 @@ export async function up (knex: Knex) {
   // tslint:disable-next-line: await-promise
   await knex.schema.createTable('user_role', table => {
     table.increments('id')
-    table.integer('userId').unsigned().index()
-    table.integer('roleId').unsigned().index()
+    table.integer('userId').unsigned().references('id').inTable('user')
+    table.integer('roleId').unsigned().references('id').inTable('role')
     table.dateTime('created').notNullable().defaultTo(knex.fn.now())
     table.dateTime('updated').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
   })
